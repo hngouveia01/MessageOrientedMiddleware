986f0e68df99bac2fd4aa419de9fd1015e0cb05c
Henrique Nascimento Gouveia <h.gouveia@icloud.com>
2016-09-09 09:29:16 -0300

    producer_default: codigo inicial
    The RabbitMQ default is to send a message and wait for knowledge.
    Here we are setting the default behaviour and sending 100000 messages.

---
+++ producer_default.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import pika
+
+# Configura os parametros de conexao para se conectar com o
+# rabbit-server na porta 5672 no host virtual / usando o
+# nome de usuario "guest" e senha "guest"
+credentials = pika.PlainCredentials('test', 'test')
+parameters = pika.ConnectionParameters('192.168.1.4',
+                                       5672,
+                                       '/',
+                                       credentials)
+
+# Cria uma conexao bloqueadora
+connection - pika.BlockingConnection(parameters)
+
+# Cria um canal de comunicacao.
+channel = connection.channel()
+
+# Declara para qual fila estara enviando mensagens.
+channel.queue_declare(queue='test_queue')
+
+count = 0
+
+# Envia mensagens
+while count != 1000000:
+  channel.basic_publish(exchange='',
+                        routing_key='test_queue',
+                        body='123456789012')
+  count += 1
+
+# Fecha conexao.
+connection.close()
